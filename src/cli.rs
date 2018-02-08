ff814548c26e8f8563d8fc9b2a8a6f1eb059b70f
Pascal Hartig <phartig@rdrei.net>
2018-02-08 16:12:14 +0000

    Read lines

---
+++ src/cli.rs
@@ -0,0 +1,16 @@
+use clap::{App, AppSettings, Arg, ArgMatches, Shell, SubCommand};
+use failure::{Error};
+
+static ABOUT: &'static str = "A tool for finding and selecting VCS revision hashes in your terminal.";
+
+pub fn cli() -> App<'static, 'static> {
+    App::new(crate_name!())
+        .setting(AppSettings::ColoredHelp)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(ABOUT)
+        .arg(Arg::with_name("FILE")
+            .required(true)
+            .help("File to read from. Pass \"-\" to capture stdin.")
+        )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+extern crate console;
+extern crate exitcode;
+extern crate failure;
+#[macro_use]
+extern crate clap;
+
+use std::io::{stderr, stdin, Write, Read, BufReader, BufRead};
+use std::fs::File;
+use std::process::{exit};
+use failure::{Error, err_msg};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    match run() {
+        Err(e) => {
+            let stderr = &mut stderr();
+            let errmsg = "Error writing to stderr.";
+            writeln!(stderr, "{}", e).expect(errmsg);
+            exit(1)
+        }
+        Ok(r) => exit(r)
+    }
+}
+
+fn run() -> Result<exitcode::ExitCode, Error> {
+    let args = cli::cli().get_matches();
+
+    let file_val = args.value_of("FILE").ok_or_else(|| err_msg("Expected FILE."))?;
+    let reader: Box<BufRead> = if file_val == "-" {
+        Box::new(BufReader::new(stdin()))
+    } else {
+        let file = File::open(file_val)?;
+        Box::new(BufReader::new(file))
+    };
+
+    for line in reader.lines() {
+        if let Ok(l) = line {
+            println!("{}", l);
+        }
+    }
+
+    Ok(exitcode::OK)
 }
