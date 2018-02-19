644c8dce75ef28a023b4f336249791ed530f0eef
Pascal Hartig <phartig@rdrei.net>
2018-02-19 22:58:51 +0000

    Redo the entire parser in a sustainable way

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use nom::{is_hex_digit, space, Err};
+use nom::{is_hex_digit, Err, hex_digit};
 use nom::types::CompleteStr;
 use failure::Error;
 
@@ -7,53 +7,40 @@ pub struct RefLike {
     pub hash: String,
 }
 
+fn mk_reflike(hash: &str) -> Option<RefLike> {
+    if hash.len() >= 6 {
+        Some(RefLike { hash: hash.to_owned() })
+    } else {
+        None
+    }
+}
+
 fn is_hex_digit_char(c: char) -> bool {
     is_hex_digit(c as u8)
 }
 
 named!(
-    reflike<CompleteStr, RefLike>,
-    do_parse!(
-        hex: map_res!(take_while!(is_hex_digit_char), from_hash) >> (RefLike {
-            hash: hex.0.trim().to_owned(),
-        })
-    )
-);
-
-fn is_not_space(c: char) -> bool {
-    c != ' ' && c != '\n' && c != '\t'
-}
-
-named!(
-    hash<CompleteStr, Option<RefLike>>,
-    do_parse!(many0!(space) >> c: opt!(reflike) >> take_while!(is_not_space) >> (c))
+    terminator<CompleteStr, CompleteStr>,
+    take_while!(|c| !is_hex_digit_char(c))
 );
 
 named!(
-    entries<CompleteStr, Vec<RefLike>>,
-    fold_many1!(
-        hash,
-        Vec::default(),
-        |mut acc: Vec<RefLike>, i| match i {
-            Some(l) => {
-                acc.push(l);
-                acc
-            }
+    token<CompleteStr, CompleteStr>,
+    terminated!(hex_digit, terminator)
+);
+
+named!(
+    tokens<CompleteStr, Vec<RefLike>>,
+    fold_many1!(token, vec![], |mut acc: Vec<RefLike>, item: CompleteStr| {
+        match mk_reflike(item.0) {
+            Some(r) => { acc.push(r); acc }
             None => acc,
         }
-    )
+    })
 );
 
-fn from_hash(input: CompleteStr) -> Result<CompleteStr, String> {
-    if input.0.len() >= 6 {
-        Ok(input)
-    } else {
-        Err("Doesn't look like a hash".into())
-    }
-}
-
 pub fn parse(l: &str) -> Result<Vec<RefLike>, Error> {
-    match entries(CompleteStr(l)) {
+    match tokens(CompleteStr(l)) {
         Ok((_remaining, value)) => { Ok(value) },
         Err(Err::Incomplete(needed)) => { bail!("Incomplete, needed: {:?}", needed) },
         Err(Err::Error(e)) | Err(Err::Failure(e)) => { bail!("Parsing failure: {:?}", e) },
@@ -65,26 +52,30 @@ mod tests {
     use nom::types::CompleteStr;
 
     #[test]
-    fn test_reflike() {
+    fn test_token() {
         assert_eq!(
-            super::reflike(CompleteStr("deadbeef")),
-            Ok((CompleteStr(""), super::RefLike { hash: "deadbeef".to_string() }))
+            super::token(CompleteStr("deadbeef")),
+            Ok((CompleteStr(""), CompleteStr("deadbeef")))
         );
+
         assert_eq!(
-            super::reflike(CompleteStr("deadberg")),
-             Ok((CompleteStr("rg"), super::RefLike { hash: "deadbe".to_string() }))
+            super::token(CompleteStr("deadbeef-faceb00c")),
+            Ok((CompleteStr("faceb00c"), CompleteStr("deadbeef")))
         );
     }
 
     #[test]
-    fn test_hashes() {
+    fn test_tokens() {
+        let result = Ok((CompleteStr(""), vec![super::RefLike { hash: "deadbeef".to_string() }, super::RefLike { hash: "aaabbbcccddd".to_string() }]));
+
         assert_eq!(
-            super::hash(CompleteStr("deadbeef")),
-            Ok((CompleteStr(""), Some(super::RefLike { hash: "deadbeef".to_string() }))));
-        // Obviously not what we actually want, but a good point of reference for future work.
+            super::tokens(CompleteStr("deadbeef-525\naaabbbcccddd")),
+            result
+        );
+
         assert_eq!(
-            super::hash(CompleteStr("hello deadbeef")),
-            Ok((CompleteStr(" deadbeef"), None))
+            super::tokens(CompleteStr("deadbeefx525zzzzaaaXXXaaabbbcccddd")),
+            result
         );
     }
 }
